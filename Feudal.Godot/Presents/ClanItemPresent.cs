764305b09979dc502241f8d78ceb78913975a702
Simon.S.Fong <Simon.S.Fong@outlook.com>
2023-11-03 16:24:03 +0800

    Add Mocks Dir

--- Feudal.Godot/Presents/ClanItemPresent.cs
+++ Feudal.Godot/Presents/ClanItemPresent.cs
@@ -9,11 +9,6 @@ namespace Feudal.Godot.Presents;
 
 internal partial class ClanItemPresent : Present<ClanItemView, ISession>
 {
-    protected override ISession MockModel { get; } = new SessionMock()
-    {
-        Clans = new List<ClanMock>() { new ClanMock() { Id = "Clan_DEFAULT", Name = "Clan_DEFAULT_Name" } }
-    };
-
     protected override void InitialConnects()
     {
 
---
+++ Feudal.Godot/Presents/Mocks/ChanItemPresent.Mock.cs
@@ -0,0 +1,12 @@
+﻿using Feudal.Interfaces;
+using System.Collections.Generic;
+
+namespace Feudal.Godot.Presents;
+
+internal partial class ClanItemPresent : Present<ClanItemView, ISession>
+{
+    protected override ISession MockModel { get; } = new SessionMock()
+    {
+        Clans = new List<ClanMock>() { new ClanMock() { Id = "Clan_DEFAULT", Name = "Clan_DEFAULT_Name" } }
+    };
+}
